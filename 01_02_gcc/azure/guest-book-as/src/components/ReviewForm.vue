8a404ad4a372a24b61ac1a7e170159eb88e5fe5d
Andrei Stefanie <andrei.stefanie@gmail.com>
2022-06-01 11:19:41 +0300

    feat(gcc): cleanup and start upload form

---
+++ 01_02_gcc/azure/guest-book-as/src/components/ReviewForm.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const file = ref<HTMLInputElement>();
+const handleFileUpload = async () => {
+  if (!file.value) {
+    return;
+  }
+  console.log('selected file', file.value.files);
+};
+</script>
+
+<template>
+  <input ref="file" v-on:change="handleFileUpload()" type="file" />
+</template>
--- 01_02_gcc/azure/guest-book-as/src/views/HomeView.vue
+++ 01_02_gcc/azure/guest-book-as/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ReviewList from '../components/ReviewList.vue';
+import ReviewForm from '../components/ReviewForm.vue';
 </script>
 
 <template>
   <main>
     <ReviewList />
+    <ReviewForm />
   </main>
 </template>
